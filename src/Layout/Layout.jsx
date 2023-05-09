0584dbe874f00835e52eaf9bb57231bbf7541563
Eunji Yoo <enjiu9932@naver.com>
2023-05-09 13:00:07 +0900

    second

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Body from './Body/Body'
+
+import React from 'react'
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer/>
+        </div>
+    )        
+}
+export default Layout
